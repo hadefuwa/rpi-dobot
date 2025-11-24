e37bda267957ca69e6e7d060ce742fed49269c82
Hamed Adefuwa <hamed.adefuwa@gmail.com>
2025-11-24 15:04:38 +0000

    Increase memory limit for main app to prevent frequent restarts

--- pwa-dobot-plc/deploy/ecosystem.config.js
+++ pwa-dobot-plc/deploy/ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '200M',
+      max_memory_restart: '400M',
       env: {
         NODE_ENV: 'production'
       },
